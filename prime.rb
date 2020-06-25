fea62d075d262f53557a1d56993a23802732f15d
jasonpatino94 <jason.patino94@gmail.com>
2020-06-25 16:02:58 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+require "pry"
+def prime?(x)
+  if x <= 0 || x == 1 
+    return false
+  else
+    for a in 2..(x-1)
+     if (x % a) == 0
+      return false
+     end
+    end
+  end
+  true
+end
+    
